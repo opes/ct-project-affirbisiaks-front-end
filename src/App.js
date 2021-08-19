7b0c5ce1afa054302cb829e53f502e344cfa6453
Kubi Kubisiak <mckubisiak@gmail.com>
2021-08-19 14:05:33 -0700

    intial react-router and compenents created for login, sign up, about, main and dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  // Redirect
+} from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Dashboard from './Components/Dashboard';
+import About from './Components/About';
+import Main from './Components/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path= '/' render={(routerProps) => <Main {...routerProps} />} />
+        
+        <Route exact path= '/login' render={(routerProps) => <Login {...routerProps} />} />
+        
+        <Route exact path= '/signup' render={(routerProps) => <Signup {...routerProps} />} />
+        
+        <Route exact path= '/dashboard' render={(routerProps) => <Dashboard {...routerProps} />} />
+        
+        <Route exact path= '/about' component={About} />
+        
+      </Switch>
+    </Router>
+    );
+  }
 }
 
-export default App;
