beeca4a62e2f8ac113d8dc329cdfee2449bf5f70
Isaac <isaac.ewing3@gmail.com>
2021-08-24 17:18:38 -0600

    made the account creation form

--- src/Components/CreateAccount.js
+++ src/Components/CreateAccount.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
 
 export default class CreateAccount extends Component {
+    state = {
+        name: '',
+        preference: '',
+        phoneNumber: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const userInfo = { 
+            name: this.state.name,
+            preference: this.state.preference,
+            phoneNumber: this.state.phoneNumber,
+            affirmations: [],
+            googleId: this.props.googleId
+         }
+         await this.props.event(userInfo)
+    }
     render() {
+        console.log(this.state);
         return (
             <div>
                 Create Account
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input name='name' placeholder='name' onChange={this.handleChange}></input>
+                    </label>
+                    <label>
+                    <input type='radio' name='preference' value='wholesome' onChange={this.handleChange}></input>
+                    </label>
+                    <label>
+                    <input type='radio' name='preference' value='motivational' onChange={this.handleChange}></input>
+                    </label>
+                    <label>
+                    <input type='radio' name='preference' value='mindfulness' onChange={this.handleChange}></input>
+                    </label>
+                    <label>
+                    <input name='phoneNumber' placeholder='phoneNumber' onChange={this.handleChange}></input>
+                    </label>
+                    <button>submit</button>
+                </form>
             </div>
         )
     }
