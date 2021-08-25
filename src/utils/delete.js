326921e5dbad8cc3000b4caed887ea35db489b9d
Sean <simpson.sean@outlook.com>
2021-08-25 15:03:57 -0700

    WORKDONE: Working Delete button

---
+++ src/utils/delete.js
@@ -0,0 +1,16 @@
+const url = 'https://affirbisiaks.herokuapp.com';
+export async function deleteUser(userInfo) {
+    console.log(userInfo);
+    const data = await fetch(`${url}/api/v1/users/${userInfo.id}`, {
+        method: 'DELETE',
+        credentials: 'include', 
+        headers: {
+            'Access-Control-Allow-Origin': '*', 
+            'Access-Control-Allow-Credentials': true 
+        },
+        body: JSON.stringify(userInfo)
+    })
+    const res = await data.json();
+
+    return res;
+}
