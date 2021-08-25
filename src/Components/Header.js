d9986a33dc0f3f9c0d1b89a0fcdf3d129ef5f3a0
Sean <simpson.sean@outlook.com>
2021-08-25 15:49:16 -0700

    WORKDONE: Created About Skeleton

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        i hated glass art being called heady
+        <h1>Affirbisiaks</h1>
       </div>
     )
   }
