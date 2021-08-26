dd0dd05897e718d6a741aeaab68bf0dd7efaeeeb
Sean <simpson.sean@outlook.com>
2021-08-26 13:30:33 -0700

    WORKDONE: Working confirmation button; Texts being delivered to devices

--- src/utils/beginService.js
+++ src/utils/beginService.js
@@ -1,6 +1,6 @@
 const url = 'https://affirbisiaks.herokuapp.com';
 export async function beginService(userInfo) {
-    const data = await fetch(`${url}/api/v1/users/send`, {
+    const data = await fetch(`${url}/api/v1/users/send/${userInfo.googleId}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
